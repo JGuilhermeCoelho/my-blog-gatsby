9f6bfbe8f24b17b49d8e7ae6c8d3fc5baadf1f34
J. Guilherme Coelho <jose.guilherme.oliveira@ccc.ufcg.edu.br>
2020-09-22 16:32:49 -0300

    first commit

---
+++ src/components/Layout/index.js
@@ -0,0 +1,32 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from "react"
+import PropTypes from "prop-types"
+import Profile from "../Profile"
+
+import * as S from "./styles"
+import GlobalStyles from "../../styles/global"
+
+
+const Layout = ({ children }) => {
+  return (
+    <S.LayoutWrapper>
+      <GlobalStyles />
+      <aside>
+        <Profile />
+      </aside>
+      <S.LayoutMain>{children}</S.LayoutMain>
+    </S.LayoutWrapper>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
